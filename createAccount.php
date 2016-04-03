1bf0537a035b9e49e968f69c92fa462f60b63a7f
mjm5qu@virginia.edu <mjm5qu@virginia.edu>
2016-04-03 17:22:13 -0400

    made some change to create account

--- createAccount.php
+++ createAccount.php
@@ -6,6 +6,6 @@
   <body>
     <h3> Please enter an email and create a password </h3>
     <form method="POST" action="create account">
-    Email <input type = "text"  
+    Email <input type = "text"; name = "email"; value=""> 
   </body>
 </html>

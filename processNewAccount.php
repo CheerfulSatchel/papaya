dfdbe62d7f5ace68ad3300921c171dedbb7c9eab
mjm5qu@virginia.edu <mjm5qu@virginia.edu>
2016-04-03 17:49:47 -0400

    hello world

--- processNewAccount.php
+++ processNewAccount.php
@@ -9,6 +9,19 @@
         $email = $_POST['email'];
         $password = $_POST['password'];
         echo "New $email and $password";
+
+        //Database parameters
+        $server = "stardock.cs.virginia.edu";
+        $username = "cs4750mjm5qu";
+        $password = "snowball";
+        $db_name = "cs4750mjm5qu";
+        $db = new mysqli("$server", "$username", "$password", "$db_name");
+
+        //Try to connect
+			  if ($db->connect_error){
+				  die("Connection error: " . $db->connect_error);
+			  }
+        
       }
     ?>
   </body>

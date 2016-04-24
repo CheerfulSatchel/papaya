ea6c18c63acf9dee263e8a3bf9d9d46137ccf41a
James <jw3qz@virginia.edu>
2016-04-23 21:12:29 -0400

    ....

--- search.php
+++ search.php
@@ -3,5 +3,18 @@
 	include 'databaseinfo.php';
 
 	if(isset($_GET['keywords'])){
-		echo $keywords = $_GET['keywords'];
+		$keywords = $db->escape_string($_GET['keywords']);
+		
+		$query = $db->query("
+			SELECT title
+			FROM video_game
+			WHERE body LIKE '%{$keywords}%'
+			OR title LIKE '%{$keywords}%' 	
+			");
+			?>
+
+			<div class="result-count">
+			Found <?php echo $query->num_rows; ?> results;
+			</div>
+
 	}

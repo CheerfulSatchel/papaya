eec9e4cfdd8a9df5006a85e2aa065d2f741ce55a
James <jw3qz@virginia.edu>
2016-04-23 21:47:32 -0400

    Finding results

--- search.php
+++ search.php
@@ -17,3 +17,14 @@
 			<div class="result-count">
 			Found <?php echo $query->num_rows; ?> results;
 			</div>
+
+<?php
+
+	if($query->num_rows){
+		while($r = $query->fetch_object()){
+		?>
+			<div class="result">
+			<a href="#"><?php echo$r->title; ?></a>
+			</div>	
+		}
+	}

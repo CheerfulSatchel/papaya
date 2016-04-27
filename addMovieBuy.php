ef8fe9bc9f3eea62679ddda891e4a6f597df8586
mjm5qu@virginia.edu <mjm5qu@virginia.edu>
2016-04-27 15:04:53 -0400

    movies add to cart

--- addMovieBuy.php
+++ addMovieBuy.php
@@ -1,13 +1,13 @@
 <?php
   include 'databaseInfo.php'; //database information to access MySQL
-  //$id = $_POST['id'];
-  //$email = $_POST['email'];
-  $id=113;
-  $email = "mjm5qu@virginia.edu";
+  $id = $_POST['id'];
+  $email = $_POST['email'];
+  //$id=113;
+  //$email = "mjm5qu@virginia.edu";
   $timestamp = "April 2016";
 
   //echo "HEYYYYYYYY";
-  $itemQuery = $db->prepare("INSERT INTO `Buy`(`email`, `item_id`, `timestamp`) VALUES (?,?,?)") or die("Your insertion couldn't be done: " . $db->error);
+  $itemQuery = $db->prepare("INSERT INTO Buy (email, item_id, timestamp) VALUES (?,?,?)") or die("Your insertion couldn't be done: " . $db->error);
   $itemQuery->bind_param("sds",$email, $id, $timestamp);
   $itemQuery->execute();
 
